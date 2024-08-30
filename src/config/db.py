3b00bf0e82dcdbb857d5cbd1de03e8fa49ee3d9a
Gonzalo1221 <115363801+Gonzalo1221@users.noreply.github.com>
2024-08-30 12:35:13 -0500

    Update db.py

--- src/config/db.py
+++ src/config/db.py
@@ -4,7 +4,7 @@ from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql+pymysql://root@localhost/crud'
+app.config['SQLALCHEMY_DATABASE_URI'] =  'mysql+pymysql://sql5728333:ZJPH4CaGFu@sql5.freesqldatabase.com/sql5728333'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 app.secret_key = "final"
